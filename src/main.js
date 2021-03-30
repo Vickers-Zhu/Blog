2e95c35ee9f559bcb831d4bd2f40bd51433421bb
vickerszhu <vickerszhu@gmail.com>
2021-03-30 17:57:59 +0800

    Initial commit (boilerplate changed)

--- src/main.js
+++ src/main.js
@@ -1,33 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui'
-import VueRouter from 'vue-router'
-import routes from './routers'
-import 'element-ui/lib/theme-chalk/index.css'
-import 'element-ui/lib/theme-chalk/display.css'
-import './assets/style.css'
-import fHeader from './components/f-header'
-import fFooter from './components/f-footer'
-import i18n from './i18n/i18n'
-
+import router from './router'
+import store from './store'
+import './assets/css/style.less'
+import './assets/font/iconfont.css'
+require('./Mock')
+import {parseTime} from './utils'
 
 Vue.config.productionTip = false
-Vue.use(ElementUI)
-Vue.use(VueRouter)
-Vue.component("f-header",fHeader)
-Vue.component("f-footer",fFooter)
-
-//Vue.use(ElementUI, {
-//i18n: (key, value) => i18n.t(key, value)
-//})
-
-const router =new VueRouter({
-	routes,
-	mode: 'history'
-})
-
+Vue.filter('parseTime', (v) => parseTime(v,'{y}-{m}-{d}'))
 new Vue({
-	router,
-	i18n,
-	render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
